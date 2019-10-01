900a6ab70036036c179edb0008c5c88166f8fab5
Ronald-Reagan1 <54944950+Ronald-Reagan1@users.noreply.github.com>
2019-10-01 09:04:52 -0500

    added new images, changed colors, added more stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
 // Put your JavaScript here
+const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+const HIDDEN_DETAIL_CLASS = 'hidden-detail';
+const TINY_EFFECT_CLASS = 'is-tiny';
+const ESC_KEY_CODE = 27;
+
+function showDetails() {
+  'use strict';
+  let frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function() {
+    frame.classList.remove(TINY_EFFECT_CLASS)
+  }, 50);
+}
+function addKeyPressHandler() {
+  'use strict';
+  document.body.addEventListener('keyup', function(event) {
+    event.preventDefault();
+    console.log(event.keyCode);
+    if (event.keyCode === ESC_KEY_CODE) {
+      hideDetails();
+    }
+  });
+}
